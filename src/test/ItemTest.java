9e762b616553af019b85ae3a4feaf45329b6d2f4
benjaminsavoy <thefrozenanthem@gmail.com>
2012-09-10 13:34:18 +0200

    added tests

---
+++ src/test/ItemTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+	// The test that always succeeds.
+	@Test
+  	public void dummy() {
+    	assertTrue(true);
+  	}
+} 
